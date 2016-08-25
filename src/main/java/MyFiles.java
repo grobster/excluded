259e600f4522b998e6bed4477b9eb39cc4d46c5f
grobster <jeff.quarles@gmail.com>
2016-08-24 23:00:15 -0400

    no change

---
+++ src/main/java/MyFiles.java
@@ -0,0 +1,22 @@
+package com.grobster.util;
+
+public class MyFiles {
+	public static String stripExtension (String str) {
+        // Handle null case specially.
+
+        if (str == null) return null;
+
+        // Get position of last '.'.
+
+        int pos = str.lastIndexOf(".");
+
+        // If there wasn't any '.' just return the string as is.
+
+        if (pos == -1) return str;
+
+        // Otherwise return the string, up to the dot.
+
+        return str.substring(0, pos);
+    }
+	
+}
